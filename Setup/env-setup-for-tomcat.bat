53748fc8567637311483e1d304f13da91d564a2e
alish49 <31355356+alish49@users.noreply.github.com>
2018-10-08 13:35:13 -0500

    Add files via upload

---
+++ Setup/env-setup-for-tomcat.bat
@@ -0,0 +1,11 @@
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_121
+
+set JRE_HOME=C:\Program Files\Java\jre1.8.0_144
+
+set PATH="C:\Program Files\Java\jdk1.8.0_121\bin";%PATH%
+
+set CLASSPATH=.;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\servlet-api.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\jsp-api.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\el-api.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\mysql-connector-java-5.1.44-bin.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\mongo-java-driver-3.2.2.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\jackson-annotations-2.9.2.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\jackson-core-2.9.2.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\jackson-databind-2.9.2.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\commons-io-2.4.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\commons-fileupload-1.3.1.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\bson-3.0.4.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\java-json.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\jersey-bundle-1.19.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\webapps\ProjectFinal\WEB-INF\lib\servlet-api.jar;
+
+set TOMCAT_HOME=C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34
+
+set CATALINA_HOME=C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34
